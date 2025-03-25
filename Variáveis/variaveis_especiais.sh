98cc029fbb9cb86931b4a788b11947b680d7c5cf
ViniciusH97 <vinicius.hl2010@gmail.com>
2025-03-25 18:48:02 -0400

    variaveis

--- Variáveis/variaveis_especiais.sh
+++ Variáveis/variaveis_especiais.sh
@@ -23,3 +23,18 @@ echo "Último comando executado: $?"
 echo "PID do script: $$"
 echo "PID do último comando executado em background: $!"
 
+# variáveis de ambiente
+echo "Diretório atual: $PWD"
+echo "Usuário atual: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Sistema operacional: $OSTYPE"
+echo "Número da linha de comando: $LINENO"
+
+# variáveis de shell
+echo "Versão do shell: $BASH_VERSION"
+echo "Histórico de comandos: $HISTFILE"
+echo "Número de comandos no histórico: $HISTSIZE"
+echo "Número de comandos no histórico de comandos: $HISTCMD"
+echo "Diretório home do usuário: $HOME"
+echo "Diretório temporário: $TMPDIR"
+echo "Nome do shell: $SHELL"
